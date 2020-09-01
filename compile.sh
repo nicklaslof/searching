c04baa1548045f0758ba1b75badbc04f51c6b4bc
Nicklas Lof <nicklaslof76@gmail.com>
2020-09-01 06:47:02 +0200

    made compile.sh to actually do everything including some manual steps

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm dist.zip
+mkdir -p dist
+cd src
+cp -av f.png ../dist/
+cp -av a-tinified.png ../dist/a.png
+cp -av *.txt ../dist/
+cp index.html index.css ../dist/
+rollup mainloop.js --format cjs --file ../dist/bundle.js
+cd ../dist
+terser bundle.js -o mainloop.js --compress --mangle --mangle-props --timings --toplevel --module --mangle-props
+rm bundle.js
+cd ..
+/c/Program\ Files/7-Zip/7z.exe a -mx9 -tzip dist.zip dist/*
+./advzip.exe  -4 -i 1000 -z dist.zip
