4d7813cb3d933186d2cbdb6496e2daf9405e4bad
Nicklas Lof <nicklaslof76@gmail.com>
2020-09-05 06:29:27 +0200

    Auto pickup

--- src/entities/itemsprite.js
+++ src/entities/itemsprite.js
@@ -17,11 +17,11 @@ collidedBy(entity, level){
     if (entity.n == "p"){
         let d = this.distanceToOtherEntity(entity);
         if(d < 1){
-            level.displayMessage("Press shift to pickup",this.n=="dagger"||this.n=="wand"?this.n+" !"+this.i.getDamage()+" damage!":this.n,0.1);
-            if (Game.inputHandler.isKeyDown(16)){
+            //level.displayMessage("Press shift to pickup",this.n=="dagger"||this.n=="wand"?this.n+" !"+this.i.getDamage()+" damage!":this.n,0.1);
+           // if (Game.inputHandler.isKeyDown(16)){
                     entity.pickup(level,this.i);
                     this.removeThisEntity(level);
-            }
+           // }
         }
     }
 }
